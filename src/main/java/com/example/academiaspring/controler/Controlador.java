3db8424eb45d1ba695fe5fa187638fb5e58273a1
Lautaro Montaña <joselautarom@gmail.com>
2023-11-15 09:32:31 -0300

    [FEATURE] Agregamos funcionalidad de agregar nuevo usuario

--- src/main/java/com/example/academiaspring/controler/Controlador.java
+++ src/main/java/com/example/academiaspring/controler/Controlador.java
@@ -7,7 +7,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping
@@ -16,12 +19,28 @@ public class Controlador {
     @Autowired
     private IpersonaService service;
     
+    //Creamos el método para listar
     @GetMapping("/listar")
     public String listar(Model model){
         List<Persona>personas=service.listar();
         model.addAttribute("personas", personas);
         return "index";
     }
+    
+    //Creamos el método para agregar new
+    @GetMapping("/new")
+    public String agregar(Model model){
+        model.addAttribute("persona", new Persona());
+        return "form";
+    }
+    
+    @PostMapping("/save")
+    public String save(@Valid Persona p, Model model){
+        service.save(p);
+        return "redirect:/listar";
+    }
+    
+    
     
     
     
